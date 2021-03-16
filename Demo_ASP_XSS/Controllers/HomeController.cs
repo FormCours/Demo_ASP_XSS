261e7415c132a674cd99d3dfe289b80f5031e8b1
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-03-16 22:43:13 +0100

    Ajoutez des fichiers projet.

---
+++ Demo_ASP_XSS/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_ASP_XSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<string> messages = new List<string>()
+        {
+            "Hello World",
+            "Ceci est une demo"
+        };
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(messages);
+        }
+
+        [HttpPost]
+        // Le framework protege contre la récéption d'injection de code "Html"
+        // ↓ Cet attribut déactive la sécurité.
+        [ValidateInput(false)] 
+        public ActionResult Index(string nouveauMessage)
+        {
+            messages.Add(nouveauMessage);
+
+            return View(messages);
+        }
+
+    }
+}
---
+++ Demo_ASP_XSS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Demo_ASP_XSS.MvcApplication" Language="C#" %>
